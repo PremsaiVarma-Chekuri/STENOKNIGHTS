20ad6175ac337223e7ef1d1941a23c17c57335be
PremsaiVarma <premsaivarmacoding@gmail.com>
2022-11-06 12:33:43 +0530

    name, age

--- extract_data.py
+++ extract_data.py
@@ -1,3 +1,8 @@
 import slate3k as slate
 text  = slate.PDF(open('C:/Users/ASUS/Downloads/md (1).pdf', 'rb')).text()
-print(text)
+#print(text)
+#print(type(text))
+import regex as re
+name = re.findall("^Name", text)
+
+#print(name)
---
+++ sorting_data_using_regex.py
@@ -0,0 +1,7 @@
+"""import sys
+sys.path.insert(1, 'C:/Users/ASUS/Desktop/STENOKNIGHTS/extract_data.py')"""
+from extract_data import *
+name = text.split('Name').pop(1).split('Age').pop(0).strip(" ").strip(":")
+print(name)
+age = text.split('Age/Sex').pop(1).split('Bill No').pop(0).strip(" ").strip(":")
+print(age)
