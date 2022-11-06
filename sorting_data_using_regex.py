7c207f6c16650ebd6c50e1cc5f56fb76fe6e20c2
PremsaiVarma <premsaivarmacoding@gmail.com>
2022-11-06 13:32:23 +0530

    extracted the details

--- sorting_data_using_regex.py
+++ sorting_data_using_regex.py
@@ -10,4 +10,10 @@ x = text
 import regex as re
 name = re.search(r'Name :(.*?)Age',text).group(1)
 age = re.search(r'Age/Sex :(.*?)Bill',text).group(1)
-print(age)
+bill_number = re.search(r'Bill No :(.*?)Ref',text).group(1)
+doctor = re.search(r'ortho Done by :(.*?)Bill',text).group(1)
+date = re.search(r'Date :(.*?)Remarks',text).group(1)
+remarks = re.search(r'Remarks :(.*?)Auth',text).group(1)
+service = re.search(r'Amount 1(.*?)[1-9.*]',text).group(1)
+amount = re.search(r'Total :(.*)',text).group(1)
+
