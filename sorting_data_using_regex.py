9464ee6008b975ad2b1b5dfe649fd80a5cb71f55
PremsaiVarma <premsaivarmacoding@gmail.com>
2022-11-06 21:36:33 +0530

    added extracting data code

--- sorting_data_using_regex.py
+++ sorting_data_using_regex.py
@@ -1,12 +1,14 @@
 """import sys
 sys.path.insert(1, 'C:/Users/ASUS/Desktop/STENOKNIGHTS/extract_data.py')"""
-from extract_data import *
+#from extract_data import *
 """name = text.split('Name').pop(1).split('Age').pop(0).strip(" ").strip(":")
 print(name)
 age = text.split('Age/Sex').pop(1).split('Bill No').pop(0).strip(" ").strip(":")
 print(age)"""
+import slate3k as slate
+text  = slate.PDF(open('C:/Users/ASUS/Downloads/md (1).pdf', 'rb')).text()
 x = text
-print(text)
+#print(text)
 # text = "xyz hospital Pat.No : 676637 Dept. : Pat.Cat : Pat.Name : sanjeev Age/Sex :18/Male Bill No :45 Ref.By : ortho Done by : robin Bill Date : 5/11/2022 Remarks : --- Auth.By: MS S.No invest igat ion / Service Name Amount 1 ortho therapy session 1,500 Signature Total : 1,500"
 import regex as re
 name = re.search(r'Name :(.*?)Age',text).group(1)
@@ -17,4 +19,5 @@ date = re.search(r'Date :(.*?)Remarks',text).group(1)
 remarks = re.search(r'Remarks :(.*?)Auth',text).group(1)
 service = re.search(r'Amount 1(.*?)[1-9.*]',text).group(1)
 amount = re.search(r'Total :(.*)',text).group(1)
+print(age)
 
